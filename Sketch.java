bfb3e646d706d6f9b5bac7e8bd9ccf1b0f7ec7bf
Lydia <75390984+iydia@users.noreply.github.com>
2022-05-21 16:07:46 +0000

    final commit w/ errors commented out

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,126 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
+	/**
+   * Assignment: 5.11 Processing Task 9 - Arrays
+   * Author: Lydia He
+   * Date: Fri, May 13, 2022
+   * Description: Create a sketch that simulates snow falling.
    */
+
+  // Variables
+  float playerX = 200;
+  float playerY = 380;
+  float[] circleX = new float[15];
+  float[] circleY = new float[15];
+  boolean[] ballHideStatus = new boolean[15];
+  int fallSpeed = 2;
+  int lives = 3;
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  background(0);
 
+  // Blue Player Circle
+  fill(37, 150, 190);
+  stroke(37, 150, 190);
+  ellipse(playerX, playerY, 20, 20);
+
+    for (int i = 0; i < circleY.length; i++) {
+      float circleX = width * i / circleY.length;
+      fill(255);
+      stroke(255);
+      ellipse(circleX, circleY[i], 20, 20);
+  
+      circleY[i] += fallSpeed;
+  
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+    }
+
+  if (keyPressed) {
+    // The snow falls slower when the up arrow is pressed
+    if (keyCode == UP) {
+      fallSpeed = 1;
+    }
+    // When the down arrow is pressed on the keyboard, the snow falls faster
+    if (keyCode == DOWN) {
+      fallSpeed = 3;
+    }
+  }
+
+  // 3 lives
+  // Add a three squares at the top right of the screen to indicate player lives.  The player loses a life everytime it collides with snowflake.  The game ends when all lives are lost and the screen clears to white.
+  if (lives == 3) {
+    fill(255, 0, 0);
+    stroke(255, 192, 203);
+    rect(380, 10, 10, 10);
+    rect(360, 10, 10, 10);
+    rect(340, 10, 10, 10);
+  }
+    
+  if (lives == 2) {
+    fill(255, 0, 0);
+    stroke(255, 192, 203);
+    rect(380, 10, 10, 10);
+    rect(360, 10, 10, 10);
+  }
+
+  if (lives == 1) {
+    fill(255, 0, 0);
+    stroke(255, 192, 203);
+    rect(380, 10, 10, 10);
+  }
+
+  // The game ends when all lives are lost and the screen clears to white.
+  if (lives == 0) {
+    fill(255);
     stroke(255);
-    line(50, 125, 70, 50);  
+    rect(0, 0, 400, 400);
   }
   
-  // define other methods down here.
+  /** Click on snowflakes and make them disappear 
+  if (mousePressed == true) {
+    if (mouseX >= circleX - 10 && mouseX <= circleX + 10 && mouseY >= circleY[i] - 10 && mouseY <= circleY[i] + 10) {
+      ballHideStatus[i] = false;
+    }
+  }
+  */
+    
+  /** The player loses a life everytime it collides with snowflake
+  if (playerX + 10 == circleX + 10 || playerY + 10 == circleY + 10) {
+    lives -=1; 
+  }
+  */
+    
+  // Controlled by the user with the asdw keys. (left, down, right , up)
+  if (key == 'w') {
+    playerY -= 2;
+  }
+
+  if (key == 'a') {
+    playerX -= 2;
+  }
+
+  if (key == 's') {
+    playerY += 2;
+  }
+
+  if (key == 'd') {
+    playerX += 2;  
+  }
+    
+}
 }
